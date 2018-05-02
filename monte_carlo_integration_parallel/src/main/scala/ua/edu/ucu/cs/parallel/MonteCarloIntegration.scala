e2ebc39e4d9675f525f22991f99b1665f5c10ef1
Irynei Baran <ibar@softserveinc.com>
2018-05-02 14:57:27 +0300

    Add another way of integral calculation

--- monte_carlo_integration_parallel/src/main/scala/ua/edu/ucu/cs/parallel/MonteCarloIntegration.scala
+++ monte_carlo_integration_parallel/src/main/scala/ua/edu/ucu/cs/parallel/MonteCarloIntegration.scala
@@ -7,57 +7,51 @@ import Math.{abs, PI, cos}
 
 object MonteCarloIntegration {
 
-  def CountPointsUnderFunction(f: Double => Double, a: Double, b:Double, min:Double, max:Double, totalNumberOfPoints: Int) = {
+  def CountPointsUnderFunction(f: Double => Double, a: Double, b:Double, totalNumberOfPoints: Int) = {
     val rndX = new Random
-    def rndY = new Random
 
-    def simulate(hits: Int, pointsGenerated: Int): Int = {
-      if (pointsGenerated >= totalNumberOfPoints) hits
+    def simulate(acc: Double, pointsGenerated: Int): Double = {
+      if (pointsGenerated >= totalNumberOfPoints) acc
       else {
         // pick up random x in range [a, b]
         val x = a + (b - a) * rndX.nextDouble()
-        val y = min + (max - min) * rndY.nextDouble()
-        if (abs(f(x)) < abs(y)) simulate(hits, pointsGenerated + 1)
-        else
-          {
-            val inc =
-              if(f(x) < 0 & y < 0) -1
-              else if (f(x) > 0 & y > 0) 1
-              else 0
-            simulate(hits + inc, pointsGenerated + 1)
-          }
+        simulate(acc + (b - a) * f(x), pointsGenerated + 1)
       }
     }
     simulate(0, 0)
   }
 
-  def integralSeq(f: Double => Double, a: Double, b:Double, min:Double, max:Double, totalNumberOfPoints: Int): Double =
-    (b - a) * (max - min) * CountPointsUnderFunction(f, a, b, min, max, totalNumberOfPoints) / totalNumberOfPoints
+  def integralSeq(f: Double => Double, a: Double, b:Double, totalNumberOfPoints: Int): Double =
+    CountPointsUnderFunction(f, a, b,totalNumberOfPoints) / totalNumberOfPoints
 
-  def integralPar(f: Double => Double, a: Double, b:Double, min:Double, max:Double, totalNumberOfPoints: Int): Double = {
-    val (p1, p2) = parallel.parallel(
-          CountPointsUnderFunction(f, a, b, min, max, totalNumberOfPoints / 2),
-          CountPointsUnderFunction(f, a, b, min, max, totalNumberOfPoints / 2)
-        )
-    (b - a) * (max - min) * (p1 + p2) / totalNumberOfPoints
+  def integralPar(f: Double => Double, a: Double, b:Double, totalNumberOfPoints: Int): Double = {
+        val (p1, p2) = parallel.parallel(
+              CountPointsUnderFunction(f, a, b, totalNumberOfPoints / 2),
+              CountPointsUnderFunction(f, a, b,  totalNumberOfPoints / 2)
+            )
+        (p1 + p2) / totalNumberOfPoints
+//    val (p1, p2, p3, p4) = parallel.parallel(
+//      CountPointsUnderFunction(f, a, b, totalNumberOfPoints / 4),
+//      CountPointsUnderFunction(f, a, b,  totalNumberOfPoints / 4),
+//      CountPointsUnderFunction(f, a, b,  totalNumberOfPoints / 4),
+//      CountPointsUnderFunction(f, a, b,  totalNumberOfPoints / 4)
+//    )
+//    (p1 + p2 + p3 + p4) / totalNumberOfPoints
   }
 
   def main(args: Array[String]): Unit = {
     val totalNumberOfPoints = 1000000
     // testing
-    def f(x: Double): Double = cos(x)
-    val (a, b) = (0, PI / 2)
-    val(min, max) = (0, 1)
+//    def f(x: Double): Double = cos(x)
+//    val (a, b) = (0, PI / 2)
 
 //    def f(x: Double): Double = x * x * x
 //    val (a, b) = (0, 1)
-//    val(min, max) = (0, 1)
 
-//    def f(x: Double): Double = x
-//    val (a, b) = (-2, 4)
-//    val(min, max) = (-2, 4)
+    def f(x: Double): Double = x
+    val (a, b) = (-2, 4)
 
-    println(integralSeq(f, a, b, min, max, totalNumberOfPoints))
+    println(integralSeq(f, a, b, totalNumberOfPoints))
 
     val standartConfig = config(
       Key.exec.minWarmupRuns -> 100,
@@ -68,11 +62,11 @@ object MonteCarloIntegration {
 
 
     val seqtime = standartConfig.measure {
-      integralSeq(f, a, b, min, max, totalNumberOfPoints)
+      integralSeq(f, a, b, totalNumberOfPoints)
     }
 
     val partime = standartConfig.measure {
-      integralPar(f, a, b, min, max, totalNumberOfPoints)
+      integralPar(f, a, b, totalNumberOfPoints)
     }
 
     println(s"sequential time $seqtime ms")
